9a1434b14343486207ce480e860567319ebe3bc7
albafcruza <broketas4@gmail.com>
2015-05-08 11:14:37 +0200

    Despliegue Lista de preguntas en Heroku

--- models/models.js
+++ models/models.js
@@ -34,6 +34,10 @@ if(count === 0) { // la tabla se inicializa solo si está vacía
 Quiz.create({ pregunta: 'Capital de Italia',
 respuesta: 'Roma'
 })
+Quiz.create({ pregunta: 'Capital de Portugal',
+respuesta: 'Lisboa'
+})
+
 .then(function(){console.log('Base de datos inicializada')});
 };
 });
