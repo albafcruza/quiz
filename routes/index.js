944b616571984bbc48d70e5f4bb5d2c4af1209bd
albafcruza <broketas4@gmail.com>
2015-05-14 11:27:04 +0200

    Crear, editar y validar pregunta

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-res.render('index', { title: 'Quiz' });
+	res.render('index', { title: 'Quiz', errors: [] });
 });
 
 
@@ -17,6 +17,10 @@ router.param('quizId', quizController.load);
 router.get('/quizes', 			quizController.index);
 router.get('/quizes/:quizId(\\d+)',	 quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', 		quizController.new);
+router.post('/quizes/create', 		quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
+router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.get('/creditos/author',		 quizController.author);
 
 module.exports = router;
